42c1805890197b0db4c9ac5dd6029c7c120c899a
Joshua Bahnsen <archrival@gmail.com>
2014-10-29 00:39:47 -0700

    Add some items to AlbumItem

--- Subsonic.Client/Items/AlbumItem.cs
+++ Subsonic.Client/Items/AlbumItem.cs
@@ -6,5 +6,7 @@
         public string Artist { get; set; }
         public int AlbumArtSize { get; set; }
         public int Rating { get; set; }
+        public int Year { get; set; }
+        public string Parent { get; set; }
     }
 }
