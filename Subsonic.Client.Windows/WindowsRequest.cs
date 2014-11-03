93fdc009ecd34bfe35504de9bb1e1d1039c3c2b9
Joshua Bahnsen <archrival@gmail.com>
2014-11-03 02:05:43 -0700

    Revert changes to ImageRequestAsync override

--- Subsonic.Client.Windows/WindowsRequest.cs
+++ Subsonic.Client.Windows/WindowsRequest.cs
@@ -20,74 +20,68 @@ namespace Subsonic.Client.Windows
 
         public override async Task<IImageFormat<T>> ImageRequestAsync(Methods method, Version methodApiVersion, SubsonicParameters parameters = null, CancellationToken? cancelToken = null)
         {
-            IImageFormat<T> image = null;
-
             var requestUri = BuildRequestUri(method, methodApiVersion, parameters);
             var request = BuildRequest(requestUri);
+            var content = new MemoryStream();
 
-            using (var content = new MemoryStream())
+            if (cancelToken.HasValue)
+                cancelToken.Value.ThrowIfCancellationRequested();
+
+            try
             {
-                if (cancelToken.HasValue)
-                    cancelToken.Value.ThrowIfCancellationRequested();
-                    
-                try
+                using (var response = await request.GetResponseAsync())
                 {
-                    using (var response = await request.GetResponseAsync())
+                    if (response != null)
                     {
-                        if (response != null)
+                        if (!response.ContentType.Contains(HttpContentTypes.TextXml))
                         {
-                            if (!response.ContentType.Contains(HttpContentTypes.TextXml))
-                            {
-                                if (cancelToken.HasValue)
-                                    cancelToken.Value.ThrowIfCancellationRequested();
+                            if (cancelToken.HasValue)
+                                cancelToken.Value.ThrowIfCancellationRequested();
 
-                                using (var stream = response.GetResponseStream())
-                                    if (stream != null)
-                                        await stream.CopyToAsync(content);
+                            using (var stream = response.GetResponseStream())
+                                if (stream != null)
+                                    await stream.CopyToAsync(content);
+                        }
+                        else
+                        {
+                            string restResponse = null;
+                            var result = new Response();
 
-                                image = new WindowsImageFormat(Image.FromStream(content)) as IImageFormat<T>;
-                            }
-                            else
-                            {
-                                string restResponse = null;
-                                var result = new Response();
+                            if (cancelToken.HasValue)
+                                cancelToken.Value.ThrowIfCancellationRequested();
 
-                                if (cancelToken.HasValue)
-                                    cancelToken.Value.ThrowIfCancellationRequested();
+                            using (var stream = response.GetResponseStream())
+                                if (stream != null)
+                                    using (var streamReader = new StreamReader(stream))
+                                        restResponse = streamReader.ReadToEnd();
 
-                                using (var stream = response.GetResponseStream())
-                                    if (stream != null)
-                                        using (var streamReader = new StreamReader(stream))
-                                            restResponse = streamReader.ReadToEnd();
+                            if (!string.IsNullOrWhiteSpace(restResponse))
+                                result = restResponse.DeserializeFromXml<Response>();
 
-                                if (!string.IsNullOrWhiteSpace(restResponse))
-                                    result = restResponse.DeserializeFromXml<Response>();
+                            if (result.ItemElementName == ItemChoiceType.Error)
+                                throw new SubsonicErrorException("Error occurred during request.", result.Item as Error);
 
-                                if (result.ItemElementName == ItemChoiceType.Error)
-                                    throw new SubsonicErrorException("Error occurred during request.", result.Item as Error);
-
-                                throw new SubsonicApiException(string.Format(CultureInfo.CurrentCulture, "Unexpected response type: {0}", Enum.GetName(typeof(ItemChoiceType), result.ItemElementName)));
-                            }
+                            throw new SubsonicApiException(string.Format(CultureInfo.CurrentCulture, "Unexpected response type: {0}", Enum.GetName(typeof(ItemChoiceType), result.ItemElementName)));
                         }
                     }
                 }
-                catch (WebException wex)
-                {
-                    var response = wex.Response as HttpWebResponse;
-                
-                    if (response != null && response.StatusCode == HttpStatusCode.NotFound)
-                        return null;
-                }
-                catch (Exception ex)
-                {
-                    throw new SubsonicApiException(ex.Message, ex);
-                }
+            }
+            catch (WebException wex)
+            {
+                var response = wex.Response as HttpWebResponse;
+
+                if (response != null && response.StatusCode == HttpStatusCode.NotFound)
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                throw new SubsonicApiException(ex.Message, ex);
             }
 
             if (cancelToken.HasValue)
                 cancelToken.Value.ThrowIfCancellationRequested();
 
-            return image;
+            return new WindowsImageFormat(Image.FromStream(content)) as IImageFormat<T>;
         }
 
         public override async Task<long> RequestAsync(string path, bool pathOverride, Methods method, Version methodApiVersion, SubsonicParameters parameters = null, CancellationToken? cancelToken = null)
