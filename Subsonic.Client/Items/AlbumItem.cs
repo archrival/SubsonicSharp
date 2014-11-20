2f0816ca44d5172e8c95b06e85475ce6952c8a1b
Joshua Bahnsen <archrival@gmail.com>
2014-11-20 02:16:16 -0700

    Fix AlbumItem property changed event handler

--- Subsonic.Client/Items/AlbumItem.cs
+++ Subsonic.Client/Items/AlbumItem.cs
@@ -1,21 +1,8 @@
-﻿using System.ComponentModel;
-using Subsonic.Client.Common.Properties;
-using System.Runtime.CompilerServices;
-
-namespace Subsonic.Client.Items
+﻿namespace Subsonic.Client.Items
 {
     public class AlbumItem : ChildItem
     {
         public string Name { get; set; }
         public string Parent { get; set; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
