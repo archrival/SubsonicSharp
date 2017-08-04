3e478c38d76bbfb05fdaad3bcbd3a91d21f4c527
Joshua Bahnsen <archrival@gmail.com>
2017-08-03 22:50:17 -0700

    Remove json

--- Subsonic.Common/Classes/MusicFolder.cs
+++ Subsonic.Common/Classes/MusicFolder.cs
@@ -1,16 +1,12 @@
-﻿using Newtonsoft.Json;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace Subsonic.Common.Classes
 {
-    [JsonObject("musicFolder")]
     public class MusicFolder
     {
-        [JsonProperty("id")]
         [XmlAttribute("id")]
         public string Id;
 
-        [JsonProperty("name")]
         [XmlAttribute("name")]
         public string Name;
     }
--- Subsonic.Common/Enums/ResponseStatus.cs
+++ Subsonic.Common/Enums/ResponseStatus.cs
@@ -1,15 +1,12 @@
-﻿using Newtonsoft.Json;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace Subsonic.Common.Enums
 {
     public enum ResponseStatus
     {
-        [JsonProperty("ok")]
         [XmlEnum("ok")]
         Ok,
 
-        [JsonProperty("failed")]
         [XmlEnum("failed")]
         Failed
     }
